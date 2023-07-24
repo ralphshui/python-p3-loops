d83b0d017adb8e27e1d7fb1d034369c12f3901e4
Ralph Shui <rskc.2112@gmail.com>
2023-07-24 15:36:14 -0400

    completed tests

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,23 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 10
+    while (i>=1):
+        print(i)
+        i-=1
+    print("Happy New Year!")
 
-def square_integers(int_list):
-    # code goes here!
-    pass
+def square_integers(int_list): 
+    int_list = [num * num for num in int_list]
+    return int_list
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for num in range(1,101):
+        if (num % 5 == 0) and (num % 3 == 0):
+            print("FizzBuzz")
+        elif num % 5 == 0:
+            print("Buzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        else:
+            print(num)
